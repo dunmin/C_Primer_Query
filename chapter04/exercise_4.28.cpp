fb74cfed41078510b94006d5e0f71b5e0b51efd4
Dgiraffe <359329298@qq.com>
2019-10-14 21:43:13 +0800

    update 4.28-4.30

---
+++ chapter04/exercise_4.28.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "bool: " << sizeof(bool) << " bytes" << endl << endl;
+	cout << "char: " << sizeof(char) << " bytes" << endl;
+	cout << "short: " << sizeof(short) << " bytes" << endl;
+	cout << "int: " << sizeof(int) << " bytes" << endl;
+	cout << "long long: " << sizeof(long long) << " bytes" << endl << endl;
+	cout << "float: " << sizeof(float) << " bytes" << endl;
+	cout << "double: " << sizeof(double) << " bytes" << endl;
+	return 0;
+}
